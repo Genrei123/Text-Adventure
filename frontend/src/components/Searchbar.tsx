ea106e435271ee396ed2a573fe65a7c08a340830
Acedo, Mark Relan Gercee <125295018+mawrk19@users.noreply.github.com>
2024-11-15 23:59:29 +0800

    added searchbar to components

---
+++ frontend/src/components/Searchbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+const Searchbar: React.FC = () => {
+  return (
+    <div className="flex items-center border border-gray-700 rounded-full p-2 w-1/4 bg-[#563C2D]">
+      <input 
+        type="text" 
+        className="flex-grow outline-none text-[#FFD700] bg-[#563C2D] ml-2 font-playfair placeholder-[#FFD700]" 
+        placeholder="Search Stories..." 
+      />
+      <FaSearch className="text-[#FFD700] mr-2" />
+    </div>
+  );
+};
+
+export default Searchbar;
