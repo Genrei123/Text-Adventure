7bf908a983fa39da442995d4e54cadad2d586f36
Catchers <119420792+Ervhyne@users.noreply.github.com>
2025-01-21 10:04:31 +0800

    Update invoiceRoutes.ts
    fixed folder name:
     "import { createInvoice } from '../controllers/paymentController';" to "import { createInvoice } from '../controllers/invoiceController';"

--- backend/src/routes/invoiceRoutes.ts
+++ backend/src/routes/invoiceRoutes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { createInvoice } from '../controllers/paymentController';
+import { createInvoice } from '../controllers/invoiceController';
 
 const router = Router();
 
