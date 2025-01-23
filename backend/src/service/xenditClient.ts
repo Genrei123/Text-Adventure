284f4d404b4f2a6ebc490b295dbb7245acdeb1a9
Ervhyne(Catcher) <119420792+Ervhyne@users.noreply.github.com>
2025-01-23 21:46:49 +0800

    repush

---
+++ backend/src/service/xenditClient.ts
@@ -0,0 +1,13 @@
+import Xendit from 'xendit-node';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secretApiKey = process.env.XENDIT_SECRET_KEY!;
+
+const xenditClient = new Xendit({
+  secretKey: secretApiKey,
+  //xenditURL: 'https://api.xendit.co' // Custom base URL
+});
+
+export const { PaymentRequest, Invoice } = xenditClient;
