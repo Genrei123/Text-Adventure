963db5c77324d32ca784cc66b7378830d24b1191
Kurosawa-Z <gianhiginofungo@gmail.com>
2025-02-26 11:20:10 +0800

    frontend
    > added contents for admin dashboard
    > responsive for mobile view
    > added a sidebar for admin dashboard

--- frontend/src/Admin/AdminDashboard.tsx
+++ frontend/src/Admin/AdminDashboard.tsx
@@ -4,52 +4,52 @@ import Sidebar from './Components/Sidebar';
 const AdminDashboard: React.FC = () => {
     return (
         <>
-        <div className="bg-[#2F2118] min-h-screen flex items-center justify-center">
-            <Sidebar/>
-            <h1 className="text-white text-2xl absolute top-[15%] left-1/2 transform -translate-x-1/2 font-cinzel">
-                Sage Dashboard
-            </h1>
+            <div className="bg-[#2F2118] min-h-screen flex flex-col items-center justify-center relative p-4 md:p-0 ">
+                <Sidebar />
+                <h1 className="text-white text-[5vh] absolute top-[10%] left-1/2 transform -translate-x-1/2 font-cinzel">
+                        Dashboard
+                </h1>
+                
+                <div className="flex flex-col md:flex-row items-center md:justify-center gap-4 mt-[25%] md:mt-0 md:ml-[5%]">
+                    {/* Total Registered Card */}
+                    <div className="bg-[#634630] rounded-lg shadow-lg p-4 h-[18vh] w-[90vw] md:w-[35vw] shadow-black flex items-center">
+                        <div className="flex flex-col items-start">
+                            <span className="text-[#B28F4C] text-4xl font-cinzel font-bold">30,000</span>
+                            <span className="text-white text-sm font-cinzel font-bold">Total Registered Players</span>
+                        </div>
+                        <img src="/Total.svg" alt="Photo Icon" className="h-10 w-10 object-cover rounded-full ml-auto" />
+                    </div>
 
+                    {/* Active Players */}
+                    <div className="bg-[#503E31] rounded-lg shadow-lg p-4 h-[16vh] w-[90vw] md:w-[18vw] shadow-black flex items-center">
+                        <div className="flex flex-col items-start">
+                            <span className="text-[#B28F4C] text-2xl font-cinzel font-bold">10,000</span>
+                            <span className="text-white text-lg font-cinzel font-bold">Active Players</span>
+                        </div>
+                        <img src="/Active.svg" alt="Photo Icon" className="h-8 w-8 object-cover rounded-full ml-auto" />
+                    </div>
+                </div>
 
+                <div className="flex flex-col md:flex-row items-center md:justify-center gap-4 mt-4 md:ml-[5%]">
+                    {/* Total Players */}
+                    <div className="bg-[#724829] rounded-lg shadow-lg p-4 h-[16vh] w-[90vw] md:w-[18vw] shadow-black flex items-center">
+                        <div className="flex flex-col items-start">
+                            <span className="text-[#B28F4C] text-2xl font-cinzel font-bold">50,000</span>
+                            <span className="text-white text-lg font-cinzel font-bold">Total Players</span>
+                        </div>
+                        <img src="/TotalP.svg" alt="Photo Icon" className="h-8 w-8 object-cover rounded-full ml-auto" />
+                    </div>
 
-
-
-
-
-
-            <div className="bg-[#634630] rounded-lg shadow-lg p-1 h-[25vh] w-[40vw] shadow-black absolute top-[22%] left-[43%] transform -translate-x-1/2 hidden md:block">
-                <div className="flex justify-end items-center h-full mr-4">
-                    <div className="flex flex-col items-start justify-center h-full ml-4"></div>
-                    <img src="/Total.svg" alt="Photo Icon" className="h-32 w-32 object-cover rounded-full" />
-                </div></div>    
-            </div>
-
-
-
-            <div className="bg-[#503E31] rounded-lg shadow-lg p-1 h-[20vh] w-[20vw] shadow-black absolute top-[22%] left-[75%] transform -translate-x-1/2 hidden md:block">
-            
-            <div className="flex justify-end items-center h-full mr-4">
-                    <img src="/Active.svg" alt="Photo Icon" className="h-10 w-10 object-cover rounded-full" />
+                    {/* Stories Created */}
+                    <div className="bg-[#482F1C] rounded-lg shadow-lg p-4 h-[16vh] w-[90vw] md:w-[18vw] shadow-black flex items-center">
+                        <div className="flex flex-col items-start">
+                            <span className="text-[#B28F4C] text-2xl font-cinzel font-bold">2,500</span>
+                            <span className="text-white text-lg font-cinzel font-bold">Stories Created</span>
+                        </div>
+                        <img src="/Stories.svg" alt="Photo Icon" className="h-8 w-8 object-cover rounded-full ml-auto" />
+                    </div>
                 </div>
             </div>
-            
-
-
-
-            <div className="bg-[#724829] rounded-lg shadow-lg p-1 h-[20vh] w-[20vw] shadow-black absolute top-[50%] left-[33%] transform -translate-x-1/2 hidden md:block">
-            <div className="flex justify-end items-center h-full mr-4">
-                    <img src="/TotalP.svg" alt="Photo Icon" className="h-10 w-10 object-cover rounded-full" />
-                </div></div>
-            
-
-
-            
-            <div className="bg-[#482F1C] rounded-lg shadow-lg p-1 h-[20vh] w-[20vw] shadow-black absolute top-[50%] left-[55%] transform -translate-x-1/2 hidden md:block">
-               <div className="flex justify-end items-center h-full mr-4">
-                    <img src="/Stories.svg" alt="Photo Icon" className="h-10 w-10 object-cover rounded-full" />
-                </div></div>
-            
-        
         </>
 
     );
--- frontend/src/Admin/Components/Sidebar.tsx
+++ frontend/src/Admin/Components/Sidebar.tsx
@@ -2,25 +2,42 @@ import React from 'react';
 
 const Sidebar: React.FC = () => {
     return (
-        
-        <div className="bg-[#1e1e1e] w-17 md:w-40 lg:w-60 h-screen fixed left-0 shadow-[10px_0_15px_-3px_rgba(0,0,0,10)] flex flex-col items-center justify-start pt-40">
-                <h1 className="text-white text-xl md:text-2xl lg:text-3xl mb-8 absolute top-20">Admin Page</h1>
+    <> 
+        <div className="bg-[#1e1e1e] w-17 md:w-40 lg:w-60 h-screen fixed left-0 shadow-[10px_0_15px_-3px_rgba(0,0,0,10)] flex flex-col items-center justify-start pt-40 hidden md:flex">
+            <h1 className="text-white text-xl md:text-2xl lg:text-3xl mb-8 absolute top-20">Admin Page</h1>
             <button className="group flex items-center w-full px-7 py-2 text-white hover:bg-[#333] mb-4">
-                <img src="/Dashboard.svg" alt="Dashboard Icon" className="w-7 h-7 group-hover:hidden" />
-                <img src="/Dashboard-after.svg" alt="Dashboard Icon Hover" className="w-7 h-7 hidden group-hover:block" />
-                <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Dashboard</span>
+            <img src="/Dashboard.svg" alt="Dashboard Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/Dashboard-after.svg" alt="Dashboard Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Dashboard</span>
             </button>
             <button className="group flex items-center w-full px-7 py-2 text-white hover:bg-[#333] mb-4">
-                <img src="/Ban.svg" alt="Banned Players Icon" className="w-7 h-7 group-hover:hidden" />
-                <img src="/Ban-after.svg" alt="Banned Players Icon Hover" className="w-7 h-7 hidden group-hover:block" />
-                <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Banned Players</span>
+            <img src="/Ban.svg" alt="Banned Players Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/Ban-after.svg" alt="Banned Players Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Banned Players</span>
             </button>
             <button className="group flex items-center w-full px-7 py-2 text-white hover:bg-[#333]">
-                <img src="/List.svg" alt="Player List Icon" className="w-7 h-7 group-hover:hidden" />
-                <img src="/List-after.svg" alt="Player List Icon Hover" className="w-7 h-7 hidden group-hover:block" />
-                <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Player List</span>
+            <img src="/List.svg" alt="Player List Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/List-after.svg" alt="Player List Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            <span className="ml-2 group-hover:text-[#C9B57B] hidden md:inline">Player List</span>
             </button>
         </div>  
+
+        <div className="bg-[#1e1e1e] w-full h-12 fixed top-0 shadow-[0_10px_15px_-3px_rgba(0,0,0,10)] flex items-center justify-around pt-10 md:hidden">
+            <h1 className="text-white text-xl mb-2 translate-y-[-40%]">Admin Page</h1>
+            <button className="group flex items-center px-4 py-1 text-white hover:bg-[#333] mb-4 -translate-y-2">
+            <img src="/Dashboard.svg" alt="Dashboard Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/Dashboard-after.svg" alt="Dashboard Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            </button>
+            <button className="group flex items-center px-4 py-1 text-white hover:bg-[#333] mb-4 -translate-y-2">
+            <img src="/Ban.svg" alt="Banned Players Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/Ban-after.svg" alt="Banned Players Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            </button>
+            <button className="group flex items-center px-4 py-1 text-white hover:bg-[#333] mb-4 -translate-y-2">
+            <img src="/List.svg" alt="Player List Icon" className="w-7 h-7 group-hover:hidden" />
+            <img src="/List-after.svg" alt="Player List Icon Hover" className="w-7 h-7 hidden group-hover:block" />
+            </button>
+        </div>
+    </>
     );
 };
 
