b84fd9a5b0cae66a04425090b4153285eb549eef
Garvs Kantohan <93835334+RensuGitHub@users.noreply.github.com>
2025-03-10 14:49:39 +0800

    Fixed the error on the database auto sync
    Fixed the service/model.ts auto sync
    Adjusted the database schema of Users
    -- Users table
    TABLE Users (
    COLUMNS Add column
    id SERIAL PRIMARY KEY
    username VARCHAR(255) UNIQUE NOT NULL
    email VARCHAR(255) UNIQUE NOT NULL
    password VARCHAR(255) NOT NULL
    private BOOLEAN NOT NULL DEFAULT true
    model VARCHAR(255) NOT NULL DEFAULT 'gpt-4':: character varying
    admin BOOLEAN NOT NULL DEFAULT false
    emailVerified BOOLEAN NOT NULL DEFAULT false
    resetPasswordToken VARCHAR(255)
    resetPasswordExpires TIMESTAMP WITH TIME ZONE
    totalCoins INTEGER NOT NULL DEFAULT O
    verificationToken VARCHAR(255)
    verificationTokenExpires TIMESTAMP WITH TIME ZONE
    createdAt TIMESTAMP WITH TIME ZONE
    updatedAt TIMESTAMP WITH TIME ZONE
    image_url VARCHAR(255)
    status VARCHAR(255) DEFAULT 'offline',
    coinsBalance INTEGER DEFAULT 0,
    subscription VARCHAR(255) DEFAULT 'Free',
    lastActivity TIMESTAMP WITH TIME ZONE
    );

--- backend/src/model/game/game.ts
+++ backend/src/model/game/game.ts
@@ -1,99 +1,93 @@
 import { DataTypes, Model, Optional } from "sequelize";
-import sequelize from "../../service/database";
+import sequelize from "../../config/sequelize";
 
 interface GameAttributes {
-    id: number;
-    title: string;
-    slug: string;
-    description: string;
-    tagline: string;
-    genre: string;
-    subgenre: string;
-    primary_color?: string;
-    prompt_name: string;
-    prompt_text?: string;
-    prompt_model?: string;
-    image_prompt_model?: string;
-    image_prompt_name?: string;
-    image_prompt_text?: string;
-    image_data?: string;
-    music_prompt_text?: string;
-    music_prompt_seed_image?: string;
-    private: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-    UserId: number | null;  // Updated to allow null temporarily
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  tagline: string;
+  genre: string;
+  subgenre?: string;
+  primary_color?: string;
+  prompt_name: string;
+  prompt_text?: string;
+  prompt_model?: string;
+  image_prompt_model?: string;
+  image_prompt_name?: string;
+  image_prompt_text?: string;
+  image_data?: string;
+  music_prompt_text?: string;
+  music_prompt_seed_image?: string;
+  private: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  UserId?: number;
 }
 
-interface GameCreationAttributes extends Optional<GameAttributes, "id" | "primary_color" | "prompt_text" | "prompt_model" | "image_prompt_model" | "image_prompt_name" | "image_prompt_text" | "image_data" | "music_prompt_text" | "music_prompt_seed_image" | "createdAt" | "updatedAt"> {}
+interface GameCreationAttributes extends Optional<GameAttributes, "id" | "subgenre" | "primary_color" | "prompt_text" | "prompt_model" | "image_prompt_model" | "image_prompt_name" | "image_prompt_text" | "image_data" | "music_prompt_text" | "music_prompt_seed_image" | "UserId"> {}
 
 class Game extends Model<GameAttributes, GameCreationAttributes> implements GameAttributes {
-    public id!: number;
-    public title!: string;
-    public slug!: string;
-    public description!: string;
-    public tagline!: string;
-    public genre!: string;
-    public subgenre!: string;
-    public primary_color?: string;
-    public prompt_name!: string;
-    public prompt_text?: string;
-    public prompt_model?: string;
-    public image_prompt_model?: string;
-    public image_prompt_name?: string;
-    public image_prompt_text?: string;
-    public image_data?: string;
-    public music_prompt_text?: string;
-    public music_prompt_seed_image?: string;
-    public private!: boolean;
-    public createdAt!: Date;
-    public updatedAt!: Date;
-    public UserId!: number | null;  // Updated to allow null temporarily
-
-    public llm_fields!: {
-        title: string;
-        description: string;
-        genre: string;
-        subgenre: string;
-        tagline: string;
-        primary_color?: string;
-    };
+  public id!: number;
+  public title!: string;
+  public slug!: string;
+  public description!: string;
+  public tagline!: string;
+  public genre!: string;
+  public subgenre?: string;
+  public primary_color?: string;
+  public prompt_name!: string;
+  public prompt_text?: string;
+  public prompt_model?: string;
+  public image_prompt_model?: string;
+  public image_prompt_name?: string;
+  public image_prompt_text?: string;
+  public image_data?: string;
+  public music_prompt_text?: string;
+  public music_prompt_seed_image?: string;
+  public private!: boolean;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public UserId?: number;
 }
 
 Game.init({
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    title: { type: DataTypes.STRING, allowNull: false },
-    slug: { type: DataTypes.STRING, allowNull: false, unique: true },
-    description: { type: DataTypes.TEXT, allowNull: false },
-    tagline: { type: DataTypes.TEXT, allowNull: false },
-    genre: { type: DataTypes.STRING, allowNull: false },
-    subgenre: { type: DataTypes.STRING, allowNull: true },
-    primary_color: { type: DataTypes.STRING, allowNull: true },
-    prompt_name: { type: DataTypes.STRING, defaultValue: "UGC", allowNull: false },
-    prompt_text: { type: DataTypes.TEXT, allowNull: true },
-    prompt_model: { type: DataTypes.STRING, allowNull: true, defaultValue: "gpt-3.5-turbo" },
-    image_prompt_model: { type: DataTypes.STRING, allowNull: true },
-    image_prompt_name: { type: DataTypes.STRING, allowNull: true },
-    image_prompt_text: { type: DataTypes.TEXT, allowNull: true },
-    image_data: { type: DataTypes.TEXT, allowNull: true },
-    music_prompt_text: { type: DataTypes.TEXT, allowNull: true },
-    music_prompt_seed_image: { type: DataTypes.STRING, allowNull: true },
-    private: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-    createdAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-    updatedAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-    UserId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,  // Changed to true temporarily
-        references: {
-            model: 'Users',
-            key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  slug: { type: DataTypes.STRING, allowNull: false, unique: true },
+  description: { type: DataTypes.TEXT, allowNull: false },
+  tagline: { type: DataTypes.TEXT, allowNull: false },
+  genre: { type: DataTypes.STRING, allowNull: false },
+  subgenre: { type: DataTypes.STRING, allowNull: true },
+  primary_color: { type: DataTypes.STRING, allowNull: true },
+  prompt_name: { type: DataTypes.STRING, allowNull: false, defaultValue: 'UGC' },
+  prompt_text: { type: DataTypes.TEXT, allowNull: true },
+  prompt_model: { type: DataTypes.STRING, allowNull: true, defaultValue: 'gpt-3.5-turbo' },
+  image_prompt_model: { type: DataTypes.STRING, allowNull: true },
+  image_prompt_name: { type: DataTypes.STRING, allowNull: true },
+  image_prompt_text: { type: DataTypes.TEXT, allowNull: true },
+  image_data: { type: DataTypes.TEXT, allowNull: true },
+  music_prompt_text: { type: DataTypes.TEXT, allowNull: true },
+  music_prompt_seed_image: { type: DataTypes.STRING, allowNull: true },
+  private: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  createdAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW, field: 'createdAt' },
+  updatedAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW, field: 'updatedAt' },
+  UserId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'Users',
+      key: 'id'
     },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE'
+  }
 }, {
-    sequelize,
-    modelName: "Game",
+  sequelize,
+  modelName: "Game",
+  tableName: "Games",
+  timestamps: true,
+  underscored: false // Disable snake_case conversion
 });
 
 export default Game;
