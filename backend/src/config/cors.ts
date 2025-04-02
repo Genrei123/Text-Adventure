ea13ac9fbcd8113f0333b9e8b3aa20226faa4497
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-04-02 22:55:26 +0800

    Added CORS of ngrok

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,8 +1,9 @@
 import { CorsOptions } from 'cors';
 
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+const ngrokUrl = process.env.COMFYUI_NGROK_URL || '""'
 const corsOptions: CorsOptions = {
-  origin: [frontendUrl, "http://localhost:5173", "https://text-adventure-six.vercel.app","https://b5fe-2405-8d40-4819-a640-b07d-662-c403-9914.ngrok-free.app"], // Specify the frontend URLs
+  origin: [frontendUrl, "http://localhost:5173", "https://text-adventure-six.vercel.app","https://b5fe-2405-8d40-4819-a640-b07d-662-c403-9914.ngrok-free.app", ngrokUrl ], // Specify the frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify only the methods you want to allow
   credentials: true, // Allow cookies to be sent with requests
   allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
